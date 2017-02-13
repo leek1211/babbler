7a9d876fdc1ab4638210dcd58ad11b76bd7cec20
leek1211 <1211leek@gmail.com>
2017-02-14 04:57:12 +0900

    Run the server with `flask run --with-threads`

--- babbler/babbler.py
+++ babbler/babbler.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
+from flask import Flask, request, g, redirect, url_for, abort, \
      render_template, flash
 from konlpy.tag import Hannanum
 import jpype
